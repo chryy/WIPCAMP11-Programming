9b130644070533ece423452ac9cad8819cc91bf8
chryy <cherryksmp@gmail.com>
2019-05-30 11:37:36 +0700

    [ADD] New Project
    morning 30/05/2019

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,29 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("ใส่ข้อมูลตรงนี้จั๊บ (ไม่ขึ้นบรรทัดใหม่)");
+        
+        System.out.print("อยู่บรรทัดนี้นะ \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("หลังจากนี้ขึ่้นใหม่ \n");
+        
+        System.out.print("อยู่บรรทัดใหม่ละ \n");
+        
+        System.out.print("เกษมาภรณื มัทฉะ แอล แชมป์ \n") ;
+         
+        System.out.println("สวัสดี เราชื่อ เชอร์รี่ ");
+        System.out.println("เราอยู่กลุ่มสีเขียวแก่ หรือรสมัทฉะ ");
+        
+           
+    }
+}
---
+++ WIP-XI/src/VI/Input/_1_Exercise.java
@@ -0,0 +1,45 @@
+
+/*
+ * WIP XI Computer Programing 
+ */
+package VI.Input;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+import java.util.Scanner;
+
+public class _1_Exercise {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        System.out.print("Enter x : ");
+
+        x = sc.nextInt();
+
+        System.out.print("Enter y : ");
+
+        y = sc.nextInt();
+
+        System.out.println("X+Y : " + (x + y));
+
+//        int xx;
+//        int yy;
+
+        System.out.print("Enter xx : ");
+        x = sc.nextInt();
+        System.out.print("Enter yy : ");
+        y = sc.nextInt();
+        System.out.println("xx+yy :" + (y - x));
+
+        {
+
+        }
+    }
+
+}
